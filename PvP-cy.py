32fcc4db98438c20a96c06c101e9b4db0d0ac341
Ilya <treyne@mail.ru>
2025-03-24 13:53:13 +0300

    Update PvP-cy.py

--- PvP-cy.py
+++ PvP-cy.py
@@ -67,7 +67,7 @@ def httpx_request(url, method, data=None):
                 try:
                     response_json = response.json()
                     debug_print("info =", response_json)
-                    return response_json
+                    return response_json,response.status_code
                 except json.JSONDecodeError as e:
                     debug_print("JSON decode error:", e)
                     return None
@@ -88,18 +88,109 @@ def account_info(con):
 
 
          
+
+
+def Auth():
+    def httpx(string):
+        import httpx
+        url = "https://node1.cl.hamsterpvp.com/prod/auth/telegram"
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "Priority": "u=4",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"}
+        body  = f"initData={string}"
+        
+        
+        try:
+            with httpx.Client(http2=True, timeout=10.0) as client:
+                response = client.post(url, headers=headers, data=body)
+            print(f"Status Code: {response.status_code}")
+            if response.content:
+                try:
+                    response_json = response.json()
+                    print('info = ', response_json)
+                    return response_json
+                    
+                except json.JSONDecodeError as e:
+                    print("JSON decode error: ", e)
+                    return None
+        except httpx.RequestError as e:
+            print(f"Ошибка сети: {e}")
+            return None
+
+
+    with open("auth.txt", "r", encoding="utf-8") as file:
+        authURL = file.read().strip()  # Читаем содержимое и удаляем лишние пробелы/переносы строк
+        url = authURL
+    # print(authURL)  # Вывод для проверки
+    # url = input("Введите строку авторизации: ")  # Ожидаем ввод от пользователя
+    
+    
+    
+
+    query_start = url.find("query_id%3D")
+
+    if query_start != -1:
+        trimmed_url = url[query_start:]
+        # print(trimmed_url)
+    else:
+        print("query_id не найден")
+        
+            
+    telegramAuth = httpx(trimmed_url)
+    token = telegramAuth["payload"]["token"]
+
+
+    import re
+    env_file = ".env"  # Имя файла .env
+
+    # Читаем содержимое .env
+    with open(env_file, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Заменяем строку Token = <значение> 
+    updated_content = re.sub(r"^Token\s*=\s*.*$", f"Token = {token}", content, flags=re.MULTILINE)
+
+    # Записываем обратно в .env
+    with open(env_file, "w", encoding="utf-8") as file:
+        file.write(updated_content)
+
+    print(f"Значение Token заменено на {token}")
+
+
+
+
+def checkAuth(con):
+    url = f"https://{con}/prod/profile/get"
+    ADS_resp = httpx_request(url, "POST", data={})  # POST с JSON
+    if ADS_resp[1] == 200:
+        print("Полная норма, продолжаем.")
+        return False
+    if ADS_resp[1] == 401:
+        print("Токен просрочен! Нужен новый.")
+        return 'start'
+
                 
            
 def ADS(con):
     url = f"https://{con}/prod/ads/rewarded/info"
-    viewsCount = httpx_request(url, "POST", data={})
-    # print (viewsCount)
+    viewsCount = httpx_request(url, "POST", data={})[0]
+    print (viewsCount)
     viewsCount = viewsCount["payload"]["rewarded"]["viewsCount"] 
     if viewsCount < 10:
         for _ in range(10 - viewsCount):
             url = f"https://{con}/prod/ads/rewarded/watched"
-            httpx_request(url, "POST", data={})
-            countdown_timer(random.randint(10, 20),'До следующей рекламы: ')
+            ADS_resp = httpx_request(url, "POST", data={})
+            if ADS_resp[1] == 200:
+                countdown_timer(random.randint(10, 20),'До следующей рекламы: ')
+            if ADS_resp[1] == 401:    
+                Auth()
     else:
         print ("Всю рекламу уже посмотрели")         
     return None
@@ -108,15 +199,28 @@ def ADS(con):
 
 def start(con, tryFlag=False):
     url = f"https://{con}/prod/profile/get"
-    AccounInfo = httpx_request(url, "POST", data={})  # POST с JSON
+    AccounInfo = httpx_request(url, "POST", data={})[0]  # POST с JSON
     energy = AccounInfo["payload"]["items"]["energy"]["count"]
     ownerId = AccounInfo["payload"]["unit"]["ownerId"]
+    energyUncommonBooster = AccounInfo["payload"]["items"]["energyUncommonBooster"]["count"]
+    print (AccounInfo["payload"]["items"]["energyUncommonBooster"]["count"])
+    # if energyUncommonBooster > 0:
+        # print (f"Применяем {energyUncommonBooster} бустеров")
+        # for _ in range(energyUncommonBooster):
+            # url = "https://node1.cl.hamsterpvp.com/prod/items/boost"
+            # httpx_request(url, "POST", data={"payload":{"boosterId":"energyUncommonBooster"}})
+            # countdown_timer(random.randint(1, 3),'До применения следующего бустера: ')
+    # else:
+        # print ("Бустеров нет!")
+
+
     countBattle = energy // 25
     if countBattle > 0:
         print (f"Число раундов = {countBattle}")
+        print (f"Энергия = {energy}")
         for _ in range(countBattle):
             url = f"https://{con}/prod/pvp/start"
-            BattleInfo = httpx_request(url, "POST", data={})  # POST с JSON
+            BattleInfo = httpx_request(url, "POST", data={})[0]  # POST с JSON
             winnerId = BattleInfo["payload"]["battle"]["winnerId"]
             if winnerId == ownerId:
                 print ("You Win!")
@@ -129,7 +233,7 @@ def start(con, tryFlag=False):
         if not tryFlag:
                 print ("Один раз пробуем на тоненького!")
                 url = f"https://{con}/prod/pvp/start"
-                BattleInfo = httpx_request(url, "POST", data={})  # POST с JSON
+                BattleInfo = httpx_request(url, "POST", data={})[0]  # POST с JSON
                 tryFlag = True
                 print (f"Энергия = {energy}")
                 return start(con, tryFlag=True)  # Передаём изменённое значение флага
@@ -141,7 +245,7 @@ def start(con, tryFlag=False):
 def balancer():
     url = "https://balancer.cl.hamsterpvp.com/connections"
     response = httpx_request(url, "GET", {})  # GET с параметрами
-    return response
+    return response[0]
     
     
 
@@ -149,6 +253,20 @@ def balancer():
 connections = balancer()
 ads_con = random.choice(connections)
 print (ads_con)
-ADS(ads_con)
+auth = checkAuth(ads_con)
+
+if auth=="start":
+    Auth()
+
+# ADS(ads_con)
 start(random.choice(connections))
 
+countdown_timer(1500,'До следующего раунда: ')
+
+while True:
+    auth = checkAuth(ads_con)
+    ADS(ads_con)
+    if auth=="start":
+        Auth()
+    start(random.choice(connections))
+    countdown_timer(1500,'До следующего раунда: ')
